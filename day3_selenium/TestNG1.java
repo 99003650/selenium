2e22588f68201451dcd0aa9f96e0132c1217b56e
99003650 <78997874+99003650@users.noreply.github.com>
2021-04-02 16:52:08 +0530

    changes

--- day3_selenium/TestNG1.java
+++ day3_selenium/TestNG1.java
@@ -1,5 +1,34 @@
-package day3_sel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class TestNG1 {
-
+	WebDriver driver;
+	
+	@Test(priority = 1)
+	public void login() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Training\\Desktop\\geckodriver.exe");  // set path of 
+	    driver = new FirefoxDriver();  
+	    driver.get("http://demo.automationtesting.in/Frames.html");
+	}
+	
+	
+	@Test(priority = 2)
+	public void max() {
+		driver.manage().window().maximize();
+	}
+	
+	@Test(priority = 3)
+	public void Single() {
+		driver.switchTo().frame("SingleFrame");
+		 driver.findElement(By.xpath("/html/body/section/div/div/div/input")).sendKeys("Omprakash");
+	}
+	
+	
+	@Test(priority = 4)
+	public void lose(){
+		driver.close();
+	}
+	
 }
